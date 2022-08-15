c715677b700f5f837503ba4c1e89715755ef0879
André Zardo <anuneszardo@gmail.com>
2022-08-15 15:36:43 -0300

    Update settings.py

--- EscolaApp/setup/settings.py
+++ EscolaApp/setup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l9nx9op24jtp!m+@c7ei2@7&82)n*(bb$+j8!cy^8xllms%3ww'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
